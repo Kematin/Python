261c90b440535237f7da42a51557af1ebe149f69
Kematin <kematin@inbox.ru>
2022-01-15 11:38:40 +0500

    Update reduce.py

--- Code Simplification/reduce.py
+++ Code Simplification/reduce.py
@@ -1,8 +1,21 @@
 from functools import reduce
-import unpacking
 
-def bad_code():
-	pass
+def previous_code():
+	x, y, z = map(int, input().strip().split())
+	return x * y * z
 
-def good_code():
-	pass
+
+def quick_code():
+	return reduce(
+		lambda x, y: x * y,
+		map(int, input().strip().split())
+		)
+
+'''
+But this code is difficult to understand
+'''
+
+
+if __name__ == '__main__':
+	volume = quick_code()
+	print(f'{volume=}')
