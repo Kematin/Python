bf8922f2d40d4a36120062aac2b23d7e9aa7606e
Kematin <kematin@inbox.ru>
2022-05-25 14:57:58 +0500

    Day tasks

--- repeat/tasks/leetcode.py
+++ repeat/tasks/leetcode.py
@@ -47,3 +47,45 @@ def is_palindrome(s: str) -> bool:
 # if is_palindrome(word):
 #     print(f'{word} = palindrome')
 # else: print(f'{word} not is palindrome')
+
+def can_construct(rans: str, mag: str) -> bool:
+    for el in rans: 
+        if el in mag:
+            mag = mag.replace(el, '', 1)
+        else: return False
+    return True
+
+# print(can_construct('aa', 'ab'))
+
+class Solution:
+    def fizzBuzz(self, n: int) -> list[str]:
+        result = list()
+        for i in range(1, n + 1):
+            if i % (15):
+                result.append('FizzBuzz')
+
+            elif i % 3 == 0: result.append('Fizz')
+            elif i % 5 == 0: result.append('Buzz')
+
+            else: result.append(str(i))
+        return result
+
+    def another_solution(self, n: int) -> list[str]: # yield
+        for i in range(1,n+1):
+            to_yield = ''
+            
+            if i % 3 == 0:to_yield += 'Fizz'
+            if i % 5 == 0:to_yield += 'Buzz'
+            elif to_yield == '': to_yield = str(i)
+            
+            yield to_yield
+
+# sol = Solution()
+# print(sol.another_solution(15))
+
+def max_value(acc: list[list[int]]) -> int:
+    result = [sum(el) for el in acc]
+    return max(result)
+
+accounts = [[2,8,7],[7,1,3],[1,9,5]]
+print(max_value(accounts))
