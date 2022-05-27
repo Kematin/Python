6e293d71623d6a48637ceeb726cb8401943bb54c
Kematin <kematin@inbox.ru>
2022-05-27 10:29:30 +0500

    Create new programm for rewiew (financial calculation)

---
+++ repeat/tasks/financial_calculation.py
@@ -0,0 +1,22 @@
+class Finance:
+
+    def is_valid(self, num):
+        if not num.isdigit(): 
+            return False
+        elif num < 0: 
+            return False
+        else: return True
+
+    def get_finance(self):
+        pass
+
+    def main(self):
+        pass
+
+
+def start():
+    pass
+
+
+if __name__ == '__main__':
+    start()
