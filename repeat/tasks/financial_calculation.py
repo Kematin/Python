6b1e193afa49fabcded711475623d70cc903caed
Kematin <kematin@inbox.ru>
2022-05-30 10:01:25 +0500

    edit financial calculation

--- repeat/tasks/financial_calculation.py
+++ repeat/tasks/financial_calculation.py
@@ -1,21 +1,39 @@
 class Finance:
 
-    def is_valid(self, num):
+    def is_num(self, num) -> bool:
         if not num.isdigit(): 
             return False
-        elif num < 0: 
+        elif int(num) < 0: 
             return False
         else: return True
 
-    def get_finance(self):
+
+    def get_num(self, text) -> int:
+        some_number = input(text)
+        while not self.is_num(some_number):
+            some_number = input(f'Введено неверное значение\n{text}')
+
+        return int(some_number)
+
+    def get_procent(self) -> str:
         pass
 
+            
     def main(self):
-        pass
+        print('\t\t\tВсе значения писать в рублях')
+        earnings = self.get_num('Ваш заработок: ')
+        food = self.get_num('Ваш расход на питание: ')
+        car = self.get_num('Ваш расход на транспортные средства: ')
+        funny = self.get_num('Ваш расход на развлечения: ')
+        utilities = self.get_num('Ваш расход на коммунальные услуги: ')
+        clothes = self.get_num('Ваш расход на одежду: ')
+        other = self.get_num('Остальные расходы: ')
 
 
 def start():
-    pass
+    calculate = Finance()
+    calculate.main()
+    # cool commentary
 
 
 if __name__ == '__main__':
