869f6b525f6d9981c3259ee31f84b35c01f8432c
Kematin <kematin@inbox.ru>
2022-06-02 13:40:17 +0500

    end work with lists, start work with tuple

--- repeat/lists/list.py
+++ repeat/lists/list.py
@@ -59,7 +59,104 @@ def methods_list():
     index_of_cat = animals.index('cat')
     print(index_of_cat) # 2
 
+    animals.pop(index_of_cat)
+    print(animals) # elephant, tiger
+
+    letters = ['a', 'b', 'c', 'd', 'a', 'a', 'b']
+    how_much_a = letters.count('a')
+    print(how_much_a) # 3
+
+    letters.sort()
+    print(letters) # a, a, a, b, b, c, d
+
+    letters.reverse()
+    print(letters) # d, c, b, b, a, a, a
+
+    del letters[4:]
+    print(letters) # d, c, b, b
+
     animals.clear()
     print(animals) # []
 
-methods_list()
+
+def why_copy_better():
+    a = [1, 2, 3]
+    b = a
+
+    b.append(4)
+    print(a) # should be 1, 2, 3, but print 1, 2, 3, 4
+    a.pop(0)
+    print(b) # should be 1, 2, 3, 4 but print 2, 3, 4
+
+
+    a = [1, 2, 3]
+    b = a.copy()
+
+    b.append(4)
+    print(a) # 1, 2, 3 
+    print(b) # 1, 2, 3, 4
+
+    a.pop(0)
+    print(a) # 2, 3
+    print(b) # 1, 2, 3, 4
+
+
+def functions_for_list():
+    numbers = [4, 2, 5, 1]
+
+    len_of_list = len(numbers)
+    print(len_of_list) # 4
+
+    sorted_numbers = sorted(numbers)
+    print(sorted_numbers) # 1, 2, 4, 5
+
+    min_element = min(numbers)
+    print(min_element) # 1
+
+    max_element = max(numbers)
+    print(max_element) # 5
+
+
+def is_element():
+    people = ['Tom', 'Alice', 'Mark']
+    try:
+        people.remove('Alice')
+        print(people)
+
+        people.remove('Bob')
+        print(people)
+    except ValueError:
+        print('Element not in list')
+
+    print('End programm')
+
+
+
+'''
+list[start:end:step]
+letters = [[0, 1, 2], [4, 5, 6]]
+five = letters[1][1]
+'''
+
+def slices_lists():
+    numbers = list(i for i in range(1, 11))
+    odd_numbers = numbers[::2]
+    even_numbers = numbers[1::2]
+    print(f'{odd_numbers}\n{even_numbers}') # 1, 3, 5, 7, 8 \n 2, 4, 6, 8, 10
+
+    peoples = [
+        ['Tom', 26],
+        ['Bob', 43],
+        ['Mark', 14],
+    ]
+    print(*peoples[1]) # Bob 43
+    print(peoples[2][0]) # Mark
+
+    for people in peoples:
+        name, age = people
+        print(f'Name: {name}\nAge: {age}')
+
+    peoples[-1].append('+7843245932')
+    print(peoples[-1]) # Mark, 14, +7843245932
+
+slices_lists()
