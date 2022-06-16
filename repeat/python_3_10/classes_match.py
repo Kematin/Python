09a5bb9894c0152941c0a4e7538da95cc106e731
kematin <kematin@inbox.ru>
2022-06-16 14:15:12 +0500

    work with pattern match

--- repeat/python_3_10/classes_match.py
+++ repeat/python_3_10/classes_match.py
@@ -4,4 +4,24 @@ class UserInput:
         self.value = value
 
 
-
+def run_horizontaly(user_input):
+    match user_input:
+        case UserInput(action='left' | 'right', value=int(value)):
+            print(f'Run horizontaly to {value}px')
+        case {'action': 'left' | 'right', 'value': int(value)}:
+            print(f'Run horizontaly to {value}px')
+        case _:
+            print('Wrong command')
+            
+input_1 = UserInput('right', 64)
+input_2 = UserInput('top', 100)
+input_3 = {
+    'id': 1591524,
+    'name': 'Robert',
+    'action': 'left',
+    'cash': 1_000_000,
+    'value': 80
+}
+inputs = input_1, input_2, input_3
+for i in inputs:
+    run_horizontaly(i)
