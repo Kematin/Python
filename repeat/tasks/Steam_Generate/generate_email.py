950d98d3919c854f7a15034ed71ff3bacd52fe9d
kematin <kematin@inbox.ru>
2022-07-14 17:56:10 +0500

    CAS-generate_email: new function: press_element, send_keys, get_data;  update main: import modules, edit main function

--- repeat/tasks/Steam_Generate/generate_email.py
+++ repeat/tasks/Steam_Generate/generate_email.py
@@ -1,11 +1,38 @@
+# Selenium 4
+from selenium import webdriver
+from selenium.webdriver.firefox.service import Service
 
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+
+# Other modules
+from time import sleep
+from generate_password import GeneratePassword as GPa
 
 class GenerateEmail:
     def __init__(self, driver):
         self.driver = driver
+        self.wait = WebDriverWait(driver, 20)
+
+    def __press_element(self, frame, xpath):
+        self.wait.until(EC.frame_to_be_available_and_switch_to_it((By.XPATH, frame)))
+        self.wait.until(EC.element_to_be_clickable((By.XPATH, xpath)))
+        self.driver.find_element(by=By.XPATH, value=xpath).click()
+        sleep(5)
+
+    def __send_keys(self, key, frame, value):
+        pass
+
+    def __get_data(self) -> list[str | str | str]:
+        pass
 
     def main(self):
-        pass
+        url = 'https://mail.rambler.ru/?utm_source=head&utm_campaign=self_promo&utm_medium=header&utm_content=mail'
+        self.driver.get(url)
+        frame = '//div[@id="root"]//iframe'
+        value = '//a[@class="styles_link___lR7s styles_link__lCdad"]'
+        self.__press_element(frame, value)
 
     def write_file(self):
         pass
@@ -15,8 +42,17 @@ def test():
     pass
 
 def start():
-    gen = GenerateEmail()
-    gen.main()
+    s = Service(executable_path='driver/geckodriver.exe')
+    driver = webdriver.Firefox(service=s)
+    try:
+        g_email = GenerateEmail(driver)
+        g_email.main()
+    except Exception as e:
+        print(e)
+    finally:
+        driver.close()
+        driver.quit()
+
 
 
 if __name__ == '__main__':
--- repeat/tasks/Steam_Generate/main.py
+++ repeat/tasks/Steam_Generate/main.py
@@ -1,11 +1,29 @@
+# Selenium 4
+from selenium import webdriver
+from selenium.webdriver.firefox.service import Service
+
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+
+# Generate
+from generate_email import GenerateEmail as GEm
+from generate_password import GeneratePassword as GPa
+from generate_phone import GeneratePhone as GPh
+from generate_steam import GenerateSteam as GSt
+
+# Other modules
+import datetime
+from time import sleep
 
 
 class Generate:
-    def __init__(self):
-        pass
+    def __init__(self, driver):
+        self.driver = driver
 
     def main(self):
-        pass
+        g_email = GEm(self.driver)
+        g_email.main()
 
     def write_csv(self):
         pass
@@ -14,9 +32,18 @@ class Generate:
 def test():
     pass
 
+
 def start():
-    gen = Generate()
-    gen.main()
+    s = Service(executable_path='driver/geckodriver.exe')
+    driver = webdriver.Firefox(service=s)
+    try:
+        gen = Generate(driver)
+        gen.main()
+    except Exception as e:
+        print(e)
+    finally:
+        driver.close()
+        driver.quit()
 
 
 if __name__ == '__main__':
